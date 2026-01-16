1f07bd2d5ea86e4df3395fc4f62749494d4e7c3a
TrevorGall16 <103269809+TrevorGall16@users.noreply.github.com>
2026-01-16 07:50:10 +0100

    footer

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-alert("SYSTEM CHECK: Connected!");
 // Use embedded data from data-embedded.js
 let itemsData = (typeof ITEMS_DATA !== 'undefined') ? ITEMS_DATA : [];
 let autocompleteTimeout = null;
